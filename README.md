3deb9fc0fa6db7a6bee60570f05dd75f7b4df511
반선호 <vsh123@naver.com>
2022-07-09 15:09:20 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# MINI FULFILLMENT
+
+KVP 스터디
+
+요구 사항 : https://docs.google.com/presentation/d/1gyS9wuVJgh3RZRJF3oWwSSerfXaxFDAVagDFLqMmujc/edit#slide=id.g135d442150d_2_77
+
+## 퀵 스타트
+
+
+## 컨벤션을 위한 ktlint 적용 
+
+```sh
+./gradlew ktlintApplyToIdea
+mkdir .git/hooks
+./gradlew addKtlintCheckGitPreCommitHook
+```
---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+
+
+plugins {
+    val kotlinVersion = "1.6.21"
+    id("org.springframework.boot") version "2.7.1"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+    kotlin("plugin.jpa") version kotlinVersion
+    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
+}
+
+group = "kvp.van"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    runtimeOnly("com.h2database:h2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("com.ninja-squad:springmockk:3.1.1")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "11"
+        }
+    }
+    test {
+        useJUnitPlatform()
+    }
+    ktlint {
+        verbose.set(true)
+    }
+}
