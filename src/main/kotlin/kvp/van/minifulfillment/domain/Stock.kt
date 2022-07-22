b820ef9697024ba173a57e7f12cfa32a586c67fd
반선호 <vsh123@naver.com>
2022-07-23 06:45:34 +0900

    feat: 재고 도메인에 Entity 추가

--- src/main/kotlin/kvp/van/minifulfillment/domain/Stock.kt
+++ src/main/kotlin/kvp/van/minifulfillment/domain/Stock.kt
@@ -1,9 +1,29 @@
 package kvp.van.minifulfillment.domain
 
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+
+@Entity
 class Stock(
+    @ManyToOne
+    @JoinColumn(name = "center_id", nullable = false)
     val center: Center,
+
+    @ManyToOne
+    @JoinColumn(name = "sku_id", nullable = false)
     val sku: Sku,
-    var quantity: Int = 0
+
+    @Column(name = "`quantity`", nullable = false)
+    var quantity: Int = 0,
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0
 ) {
     init {
         require(quantity >= 0) { "재고는 0보다 작을 수 없습니다." }
